d179175c8998c77a2809dc0fc4d1c757b16d0065
RVK10601999 <62978278+RVK10601999@users.noreply.github.com>
2021-05-19 23:53:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pickle as pkl
 import numpy as np
 import pandas as pd
 import sklearn
-import xgboost
 app = Flask(__name__)
 mod = pkl.load(open("mod.pkl",'rb'))
 cols = ['size', 'os', 'ram', 'gprm', 'fp', 'Touch_or_not', 'wid', 'hgt', 'usb2','usb3', 'usbc', 'ethr', 'blth', 'strtype', 'amst', 'ncrs', 'thrds','bc', 'mcl', 'pnm']
